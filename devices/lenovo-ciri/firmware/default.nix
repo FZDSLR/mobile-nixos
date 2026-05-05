acf79755a72aa6b569e1fe88d4b0661012b65c9a
fzdslr-nw <fzdslr@qq.com>
2026-05-05 06:08:44 +0000

    try to fix

--- devices/lenovo-ciri/firmware/default.nix
+++ devices/lenovo-ciri/firmware/default.nix
@@ -4,14 +4,19 @@
 }:
 
 # The minimum set of firmware files required for the family
-runCommand "mt8183-chromeos-firmware" {
+runCommand "mt8188-chromeos-firmware" {
   src = linux-firmware;
   meta.license = linux-firmware.meta.license;
 } ''
   for firmware in \
     mediatek/mt8188/scp.img \
+    mediatek/mt8188/sof/sof-mt8188.ldc \
+    mediatek/mt8188/sof/sof-mt8188.ri \
+    mediatek/sof-tplg/sof-mt8188.tplg \
   ; do
     mkdir -p "$(dirname $out/lib/firmware/$firmware)"
     cp -vrf "$src/lib/firmware/$firmware" $out/lib/firmware/$firmware
   done
+
+  cp -v $out/lib/firmware/mediatek/mt8188/scp.img $out/lib/firmware/mediatek/mt8188/scp_c0.img
 ''
