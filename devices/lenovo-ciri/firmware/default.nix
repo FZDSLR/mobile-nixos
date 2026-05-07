ea52d2954eaa6ff1bc051b3f4b93f8d4e2ed9257
fzdslr-nw <fzdslr@qq.com>
2026-05-07 17:19:55 +0000

    update config and firmware

--- devices/lenovo-ciri/firmware/default.nix
+++ devices/lenovo-ciri/firmware/default.nix
@@ -13,6 +13,8 @@ runCommand "mt8188-chromeos-firmware" {
     mediatek/sof/sof-mt8188.ldc \
     mediatek/sof/sof-mt8188.ri \
     mediatek/sof-tplg/sof-mt8188.tplg \
+    mediatek/WIFI_MT7961_patch_mcu_1_2_hdr.bin \
+    mediatek/WIFI_RAM_CODE_MT7961_1.bin \
   ; do
     mkdir -p "$(dirname $out/lib/firmware/$firmware)"
     cp -vrf "$src/lib/firmware/$firmware" $out/lib/firmware/$firmware
