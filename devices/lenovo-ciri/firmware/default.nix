db07d28ba9d8d3269e6d88887ccdbefcbef54a6a
fzdslr-nw <fzdslr@qq.com>
2026-05-09 06:31:57 +0000

    update firmware

--- devices/lenovo-ciri/firmware/default.nix
+++ devices/lenovo-ciri/firmware/default.nix
@@ -8,17 +8,32 @@ runCommand "mt8188-chromeos-firmware" {
   src = linux-firmware;
   meta.license = linux-firmware.meta.license;
 } ''
+  # Copy all required firmware except mediatek/mt8188/scp*.img (handled separately)
   for firmware in \
-    mediatek/mt8188/scp.img \
     mediatek/sof/sof-mt8188.ldc \
     mediatek/sof/sof-mt8188.ri \
     mediatek/sof-tplg/sof-mt8188.tplg \
     mediatek/WIFI_MT7961_patch_mcu_1_2_hdr.bin \
     mediatek/WIFI_RAM_CODE_MT7961_1.bin \
   ; do
-    mkdir -p "$(dirname $out/lib/firmware/$firmware)"
-    cp -vrf "$src/lib/firmware/$firmware" $out/lib/firmware/$firmware
+    mkdir -p "$(dirname "$out/lib/firmware/$firmware")"
+    cp -vrf "$src/lib/firmware/$firmware" "$out/lib/firmware/$firmware"
   done
 
-  cp -v $out/lib/firmware/mediatek/mt8188/scp.img $out/lib/firmware/mediatek/mt8188/scp_c0.img
+  # Handle scp firmware: prefer scp.img, fallback to scp_c0.img
+  src_dir="$src/lib/firmware/mediatek/mt8188"
+  dst_dir="$out/lib/firmware/mediatek/mt8188"
+  mkdir -p "$dst_dir"
+
+  if [ -f "$src_dir/scp.img" ]; then
+    scp_src="$src_dir/scp.img"
+  elif [ -f "$src_dir/scp_c0.img" ]; then
+    scp_src="$src_dir/scp_c0.img"
+  else
+    echo "Error: neither scp.img nor scp_c0.img found in $src_dir" >&2
+    exit 1
+  fi
+
+  cp -v "$scp_src" "$dst_dir/scp.img"
+  cp -v "$scp_src" "$dst_dir/scp_c0.img"
 ''
