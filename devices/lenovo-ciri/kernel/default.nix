1058ac34c24e47db99cdc5283848f661e7a910b6
fzdslr-nw <fzdslr@qq.com>
2026-07-30 09:54:08 +0000

    add patch

--- devices/lenovo-ciri/kernel/default.nix
+++ devices/lenovo-ciri/kernel/default.nix
@@ -43,6 +43,11 @@ mobile-nixos.kernel-builder {
       url = "https://lore.kernel.org/all/770d36b07311bf88210c187923f243fb9f126f04.1777058551.git.pav@iki.fi/raw";
       hash = "sha256-mwSnx1mV1b81xENbK9YQMlu8phzwKU0743QfUMky3jM=";
     })
+    # [PATCH] media: mtk-jpeg: cancel workqueue on release for supported platforms only
+    (fetchurl {
+      url = "https://lore.kernel.org/all/20260401-mtk-jpeg-release-issue-v1-1-2271a1779340@collabora.com/raw";
+      hash = "sha256-YLhgGbVxcrlnbcSMrcbSFNMnnbiVhApRbUzaJzoedIU=";
+    })
     ./pcie_wifi_bt.diff
     ./0001-HID-Add-hid-himax-from-chromiumos-third_party-kernel.patch
     ./0001-HID-hid-himax-master-controller.patch
