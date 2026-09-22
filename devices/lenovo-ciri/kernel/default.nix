d966593aa0967fc4b4dd529597e8ce19762c249f
fzdslr <fzdslr@qq.com>
2026-09-22 16:08:50 +0800

    test: clean maybe unused patches

--- devices/lenovo-ciri/kernel/default.nix
+++ devices/lenovo-ciri/kernel/default.nix
@@ -22,16 +22,6 @@ mobile-nixos.kernel-builder {
       url = "https://lore.kernel.org/all/20260504072846.2581096-1-zhengxingda@iscas.ac.cn/raw";
       hash = "sha256-dgVOnNXBsMNOxlkiSImgrAGXw35kITN3nkM2QZSj5Ks=";
     })
-    # [PATCH v3 3/4] HID: multitouch: use __free(kfree) to clean up
-    (fetchurl {
-      url = "https://lore.kernel.org/linux-input/20260504-wip-fix-core-v3-3-ce1f11f4968f@kernel.org/raw";
-      hash = "sha256-WFhmmvHn1wtdC5m2I73GaWkEF75aY0JeyFSFTdJ2+7w=";
-    })
-    # [PATCH wireless] wifi: mt76: mt7921: check drv_pmctrl return in the PCIe reset path
-    (fetchurl {
-      url = "https://lore.kernel.org/all/20260809012309.43657-1-lucid_duck@justthetip.ca/raw";
-      hash = "sha256-cpIh+ZJOldp7sMRgpdLz5ou2UbexNLAfAdBNIB1Pbto=";
-    })
     # [PATCH] arm64: dts: mediatek: mt8188-geralt: Enlarge SCP core0 memory region
     (fetchurl {
       url = "https://lore.kernel.org/all/20260824090409.203230-1-justin.yeh@mediatek.com/raw";
